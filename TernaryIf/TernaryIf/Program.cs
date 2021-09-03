9b21e11fb6836c600f154aa95affee54a571d522
Marcus Medina <medina.jobb@outlook.com>
2021-09-03 12:43:27 +0200

    La till TernaryIf exempel

---
+++ TernaryIf/TernaryIf/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TernaryIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en nuffra");
+            string input = Console.ReadLine();
+            int value = 0;
+            bool isOK = int.TryParse(input, out value);
+            if (isOK)
+            {
+                string message = (value > 10 ? "Större än 10" : "Inte större än 10");
+                Console.Write("Ditt värde är " + message);
+            }
+        }
+    }
+}
