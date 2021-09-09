32fafb4ea71913c2cea8156444202b5102e2b0a1
Marcus Medina <medina.jobb@outlook.com>
2021-09-09 15:09:32 +0200

    Daily planet banner

--- OOP Grund/ListExempel/StringListDemo/Program.cs
+++ OOP Grund/ListExempel/StringListDemo/Program.cs
@@ -20,8 +20,18 @@
                 "Ron Troupe - Political Columnist and editor",
                 "Steve Lombard - Sports Columnist and editor"
             };
-            
-            Console.WriteLine("Daily Planet staff");
+
+
+            Console.WriteLine(@" _____        _ _             _                  _   ");
+            Console.WriteLine(@"|  __ \      (_) |           | |                | |  ");
+            Console.WriteLine(@"| |  | | __ _ _| |_   _ _ __ | | __ _ _ __   ___| |_ ");
+            Console.WriteLine(@"| |  | |/ _` | | | | | | '_ \| |/ _` | '_ \ / _ \ __|");
+            Console.WriteLine(@"| |__| | (_| | | | |_| | |_) | | (_| | | | |  __/ |_ ");
+            Console.WriteLine(@"|_____/ \__,_|_|_|\__, | .__/|_|\__,_|_| |_|\___|\__|");
+            Console.WriteLine(@"                   __/ | |                           ");
+            Console.WriteLine(@"                  |___/|_|                           ");
+
+            Console.WriteLine("Staff:");
             foreach (var employee in dailyPlanetEmployees)
             {
                 Console.WriteLine(employee);
