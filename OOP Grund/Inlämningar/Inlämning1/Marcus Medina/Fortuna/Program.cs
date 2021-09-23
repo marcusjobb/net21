95c05998c1b439aa30e6029580f8545c1fd8d5df
Marcus Medina <medina.jobb@outlook.com>
2021-09-23 22:18:09 +0200

    Snyggade upp koden lite

--- OOP Grund/Inlämningar/Inlämning1/Marcus Medina/Fortuna/Program.cs
+++ OOP Grund/Inlämningar/Inlämning1/Marcus Medina/Fortuna/Program.cs
@@ -2,26 +2,30 @@
 {
     using System;
 
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            if (args is null)
+            {
+                throw new ArgumentNullException(nameof(args));
+            }
+
             int bank = 500;
-            int bet = 0;
-            int lucky = 0;
-            string input = "";
 
             // Loop 1 - Spelet
             while (true)
             {
                 Console.Clear();
                 Console.WriteLine("Ditt saldo är " + bank + " pix");
+                int bet;
+                string input;
                 // Loop 2 - Hur mycket du vill satsa
                 while (true)
                 {
                     Console.Write("Hur mycket vill du satsa? ");
                     input = Console.ReadLine();
-                    int.TryParse(input, out bet);
+                    _ = int.TryParse(input, out bet);
                     if (bet < 50)
                     {
                         Console.WriteLine("Du valde för litet värde");
@@ -36,12 +40,13 @@
                     }
                 }
 
+                int lucky;
                 // Loop 2 - Vad är ditt lyckotal
                 while (true)
                 {
                     Console.Write("Vad är ditt lyckotal? ");
                     input = Console.ReadLine();
-                    int.TryParse(input, out lucky);
+                    _ = int.TryParse(input, out lucky);
                     if (lucky < 1)
                     {
                         Console.WriteLine("Du valde för litet värde");
@@ -57,7 +62,7 @@
                 }
                 bank -= bet;
                 Console.WriteLine("Kastar tärningar");
-                Random rnd = new Random();
+                Random rnd = new();
                 int t1 = rnd.Next(1, 7);
                 int t2 = rnd.Next(1, 7);
                 int t3 = rnd.Next(1, 7);
@@ -66,12 +71,12 @@
                 Console.WriteLine($"Tärning 3 {t3}");
                 int multi = 0;
                 if (t1 == lucky && t2 == lucky && t3 == lucky) multi = 4;
-                else if (t1 == lucky && t2 == lucky || t2 == lucky && t3 == lucky) multi = 3;
+                else if ((t1 == lucky && t2 == lucky) || (t2 == lucky && t3 == lucky)) multi = 3;
                 else if (t1 == lucky || t2 == lucky || t3 == lucky) multi = 2;
 
                 if (multi > 0)
                 {
-                    Console.WriteLine("Du vann " + bet * multi);
+                    Console.WriteLine("Du vann " + (bet * multi));
                     bank += bet * multi;
                 }
 
