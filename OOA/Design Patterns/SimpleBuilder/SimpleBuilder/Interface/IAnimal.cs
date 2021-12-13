23389b6fd673196cd84009c26065345a93b7d641
Marcus Medina <medina.jobb@outlook.com>
2021-12-13 23:55:19 +0100

    Automatic Commit 1

---
+++ OOA/Design Patterns/SimpleBuilder/SimpleBuilder/Interface/IAnimal.cs
@@ -0,0 +1,17 @@
+﻿// -----------------------------------------------------------------------------------------------
+//  IAnimal.cs by Marcus Medina, Copyright (C) 2021, Codic Education AB.
+//  Published under GNU General Public License v3 (GPL-3)
+// -----------------------------------------------------------------------------------------------
+
+namespace SimpleBuilder.Interface
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class IAnimal
+    {
+    }
+}
