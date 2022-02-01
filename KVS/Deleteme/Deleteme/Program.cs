828e8d88959aded1a4734e4fa79b7058efca8cf2
Marcus Medina <medina.jobb@outlook.com>
2022-02-01 20:00:06 +0100

    Automatic Commit 1

--- KVS/Deleteme/Deleteme/Program.cs
+++ KVS/Deleteme/Deleteme/Program.cs
@@ -1,39 +1,55 @@
-﻿// open connection to Kitten database on MongoDB localhost
+﻿using System.Globalization;
+using System.ComponentModel.DataAnnotations;
+
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+var kitten = new KittenBuilder("Bastimisse")
+    .SetColor("Black", "Brown", "Black")
+    .AddKid(new KittenBuilder("Bastimissa").SetColor("Black", "Black", "BlackWhite").Build());
+kitten.Save();
+
+kitten = new KittenBuilder("Atreyo").SetColor("Striped grey","green");
+kitten.Save();
+
+Console.WriteLine(kitten.Build().Name);
 
 // Create a builder class for Kittens
-using MongoDB.Driver;
 
 public class KittenBuilder
 {
     Kitten myKitten = new();
-    public string DBName { get; set; } = "Kittens";
-    public string Collection { get; set; } = "Kittens";
-    public string ConnectionString { get; set; } = "MongoDB://localhost:27017";
-
-    public Kittenbuilder(string name)
+    KittenCRUD crud = new();
+    public KittenBuilder()
     {
-        // Fetch the kitten from MongoDB or create a new one
-        var client = new MongoClient(ConnectionString);
-        var database = client.GetDatabase(DBName);
-        var collection = database.GetCollection<Kitten>(collection);
-        var kitten = collection.Find(c => c.Name == name).FirstOrDefault();
-        if (kitten == null)
+
+    }
+    public KittenBuilder(string name)
+    {
+        var kitten = crud.Get(name);
+
+        if(kitten == null)
         {
-            kitten = new Kitten();
-            kitten.Name = name;
+            myKitten = new Kitten();
+            myKitten.Name = name;
+        }
+        else
+        {
+            myKitten = kitten;
         }
     }
-    public Kittenbuilder(Guid id)
+    public KittenBuilder(Guid id)
     {
-        // Fetch the kitten from MongoDB or create a new one
-        var client = new MongoClient(ConnectionString);
-        var database = client.GetDatabase(DBName);
-        var collection = database.GetCollection<Kitten>(collection);
-        var myKitten = collection.Find(c => c.Id == id).FirstOrDefault();
-        if (myKitten == null)
+        var kitten = crud.Get(id);
+
+        if(kitten == null)
         {
             myKitten = new Kitten();
             myKitten.Id = id;
+        }
+        else
+        {
+            myKitten = kitten;
         }
     }
 
@@ -43,13 +59,20 @@ public class KittenBuilder
         return this;
     }
 
-    public KittenBuilder SetColor(string fur, string eyes, string tail)
-     {
-        myKitten.FurColor = fur;
-        myKitten.EyesColor = eyes;
-        myKitten.TailColor = tail;
+    public KittenBuilder SetColor(string fur, string eyes)
+    {
+        myKitten.Color.FurColor = fur;
+        myKitten.Color.EyesColor = eyes;
+        myKitten.Color.TailColor = fur;
         return this;
-    };
+    }
+    public KittenBuilder SetColor(string fur, string eyes, string tail)
+    {
+        myKitten.Color.FurColor = fur;
+        myKitten.Color.EyesColor = eyes;
+        myKitten.Color.TailColor = tail;
+        return this;
+    }
 
     public KittenBuilder AddKid(Kitten kid)
     {
@@ -59,61 +82,122 @@ public class KittenBuilder
 
     public Kitten Build()
     {
-        // convert kitten to KittenDB format then
-        // update or save kitten data to MongoDB
-        var client = new MongoClient(ConnectionString);
-        var database = client.GetDatabase(DBName);
-        var collection = database.GetCollection<KittenDB>(collection);
-        var kittenDb = ToKittenDB(myKitten);
-
         return myKitten;
     }
 
-public static KittenDB ToKittenDB(Kitten kitten)
+    public KittenBuilder Save()
     {
-        return new KittenDB()
-        {
-            Name = kitten.Name,
-            Id = kitten.Id,
-            Color = kitten.Color,
-            Kids = kitten.Kids,
-            ColorId = kitten.Color.Id,
-            KidsId = kitten.Kids.Select(k => k.Id).ToArray()
-        };
+        crud.Save(myKitten);
+        return this;
+    }
 
-    public static Kitten ToKitten(KittenDB kitten)
-    {
-        return new Kitten()
-        {
-            Name = kitten.Name,
-            Id = kitten.Id,
-            Color = kitten.Color,
-            Kids = kitten.Kids,
-            //ColorId = kitten.Color.Id,
-            //KidsId = kitten.Kids.Select(k => k.Id).ToArray()
-        };
-    }
-    }
+
 }
 
-class KittenDB:Kitten
+public class KittenDB 
 {
+    public Guid Id { get; set; } = Guid.NewGuid();
+    public string Name { get; set; } = "";
     public Guid[] KidsId { get; set; }
     public Guid ColorId { get; set; }
 }
-class Kitten
+public class Kitten
 {
-    public Guid Id { get; set; }= Guid.NewGuid();
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = "";
     public List<Kitten> Kids { get; set; } = new();
     public Color Color { get; set; } = new();
 }
 
-public class Color 
+public class Color
 {
-    public Guid Id { get; set; }
+    public Guid Id { get; set; } = Guid.NewGuid();
     public string FurColor { get; set; }
-    public string EyeColor { get; set; }
+    public string EyesColor { get; set; }
     public string TailColor { get; set; }
 }
 
+// create a CRUD class for Kittens on MongoDB
+public class KittenCRUD
+{
+    // Define database connection
+    private readonly MongoClient _client;
+    private readonly IMongoDatabase _database;
+    private readonly IMongoCollection<KittenDB> _colKittenDB;
+    private readonly IMongoCollection<Kitten> _colKitten;
+
+    public KittenCRUD()
+    {
+        _client = new MongoClient("mongodb://localhost:27017");
+        _database = _client.GetDatabase("Kittens");
+        _colKittenDB = _database.GetCollection<KittenDB>("Kittens");
+        _colKitten = _database.GetCollection<Kitten>("Kittens");
+    }
+    public static KittenDB ToKittenDB(Kitten kitten)
+    {
+        return new KittenDB()
+        {
+            Name = kitten.Name,
+            Id = kitten.Id,
+            ColorId = kitten.Color.Id,
+            KidsId = kitten.Kids.Select(k => k.Id).ToArray()
+        };
+    }
+
+    public Kitten Get(Guid id)
+    {
+        var kitten = _colKittenDB.Find(k => k.Id == id).FirstOrDefault();
+        if(kitten == null)
+        {
+            return null;
+        }
+        return KittyTransformer(kitten);
+    }
+    public Kitten Get(string name)
+    {
+        var kitten = _colKittenDB.Find(c => c.Name == name).FirstOrDefault();
+        return KittyTransformer(kitten);
+    }
+
+    private Kitten KittyTransformer(KittenDB kitten)
+    {
+        if(kitten == null)
+            return null;
+        var color = _database.GetCollection<Color>("Colors").Find(c => c.Id == kitten.ColorId).FirstOrDefault();
+        var kidsId = kitten.KidsId.ToArray();
+        var kids = _colKitten.AsQueryable<Kitten>().Where(c => kidsId.Contains(c.Id)).ToList();
+        return new Kitten()
+        {
+            Name = kitten.Name,
+            Id = kitten.Id,
+            Color = color,
+            //Kids = new List<Kitten>(kids)
+        };
+    }
+
+    public void Save(Kitten kitten)
+    {
+        var kittenDb = ToKittenDB(kitten);
+        var color = _database.GetCollection<Color>("Colors").Find(c => c.Id == kittenDb.ColorId).FirstOrDefault();
+        if (color==null)
+            color = _database.GetCollection<Color>("Colors").Find(c => c.EyesColor == kitten.Color.EyesColor && c.FurColor == kitten.Color.FurColor && c.TailColor== kitten.Color.TailColor).FirstOrDefault();
+        if(color == null)
+            _database.GetCollection<Color>("Colors").InsertOne(kitten.Color);
+        else
+            _database.GetCollection<Color>("Colors").ReplaceOne(c => c.Id == kittenDb.ColorId, kitten.Color);
+
+        var kidCollection = _database.GetCollection<KittenDB>("Kittens");
+        foreach(var kid in kitten.Kids)
+        {
+            Save(kid);
+        }
+
+        var kit = _colKittenDB.Find(c => c.Id == kittenDb.Id).FirstOrDefault();
+        if(kit == null)
+            kit = _colKittenDB.Find(c => c.Name == kitten.Name).FirstOrDefault();
+        if(kit == null)
+            _colKittenDB.InsertOne(kittenDb);
+        else
+            _colKittenDB.ReplaceOne(c => c.Id == kittenDb.Id, kittenDb);
+    }
+}
