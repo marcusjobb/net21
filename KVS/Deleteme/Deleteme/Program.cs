0779962529c56f22e8d3e20d4166955cdb4259a9
Marcus Medina <medina.jobb@outlook.com>
2022-02-02 23:04:09 +0100

    Ignored files cleaned

--- KVS/Deleteme/Deleteme/Program.cs
+++
@@ -1,7 +0,0 @@
-using Deleteme;
-var qd = new QuickAndDirtyNew();
-// qd.AdaptPropertiesToMongoDB(@"C:\Users\marcu\source\MMPro\DailyNotes\");
-//qd.NicerProperties(@"C:\Users\marcu\OneDrive - Software Skills International AB\Dokument\Net21\GitHub\KVS\Deleteme\Deleteme\Models\");
-qd.AdaptPropertiesToMongoDB(@"C:\Users\marcu\OneDrive - Software Skills International AB\Dokument\Net21\GitHub\KVS\Deleteme\Deleteme\Models\");
-qd.TidyUpDocument(@"C:\Users\marcu\OneDrive - Software Skills International AB\Dokument\Net21\GitHub\KVS\Deleteme\Deleteme\Models\");
-qd.TidyUpDocument(@"C:\Users\marcu\OneDrive - Software Skills International AB\Dokument\Net21\GitHub\KVS\Deleteme\");
