91ab0f7bcb78ac317dda6340b2e3a01175d496d5
Marcus <medina.jobb@outlook.com>
2022-02-21 13:41:40 +0100

    HTML & Js demos

---
+++ KVS/ExpandoObject/ExpandoObject/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Dynamic;
+
+Console.WriteLine("Hello, Expando!");
+
+dynamic person = new ExpandoObject();
+person.Name = "Clark";
+person.Lastname = "Kent";
+
+Console.WriteLine(person.Name+" "+person.LastName);
