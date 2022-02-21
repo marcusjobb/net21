2ada5778b3f5a54313da92704d75c26cec0c1f64
Marcus <medina.jobb@outlook.com>
2022-02-21 09:37:11 +0100

    Dynamic object

---
+++ KVS/DynamicStuff/DynamicStuff/Program.cs
@@ -0,0 +1,20 @@
+﻿Add("Hello", "World");
+Add("Twenty=", 20);
+Add(3.1415, "is Pi");
+Add(3.32, 25);
+Add(8.25, 0);
+static void Add (dynamic dyn1, dynamic dyn2)
+{
+    var add = dyn1 + dyn2;
+
+    Console.WriteLine($"{dyn1} {dyn2}");
+    Console.WriteLine(add);
+    if (!(dyn1 is string || dyn2 is string))
+    {
+        var div = dyn1 / dyn2;
+        Console.WriteLine(div);
+    }
+    Console.WriteLine($"Variabeln dyn1 is {dyn1.GetType()}");
+    Console.WriteLine($"Variabeln dyn2 is {dyn2.GetType()}");
+    Console.WriteLine();
+}
